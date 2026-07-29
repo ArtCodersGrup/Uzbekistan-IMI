cda2addb2948602836ba087e839a90d04ce0572e
ArtCodersGrup <96072204+ArtCodersGrup@users.noreply.github.com>
2026-07-29 13:40:21 +0500

    Supabase integratsiyasi frontend bilan yakunlandi

---
+++ supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://obswrerbtrznxhcvridd.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_1UKiU7N6x0AGdeeOybMXHg_6iRYCuiN';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
